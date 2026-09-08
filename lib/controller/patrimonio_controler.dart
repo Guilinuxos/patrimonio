795d81fbc5fec182306085a630b7f98a755b5fb1
Guilherme Barros <guilhermecassimiro425@gmail.com>
2026-09-08 14:39:27 -0300

    inicio da controler

--- lib/controller/patrimonio_controler.dart
+++ lib/controller/patrimonio_controler.dart
@@ -1,3 +1,30 @@
 import 'package:get/get.dart';
+import '../model/patrimonios.dart';
+import '../service/patrimonios_service.dart';
 
-class PatrimonioControler extends GetxController {}
+class PatrimonioControler extends GetxController {
+
+  final PatrimoniosService service = PatrimoniosService();
+
+  var patrimonios = <Patrimonios>[].obs;
+  var carregando = false.obs;
+
+  @override
+  void onInit(){
+    super.onInit();
+
+    buscarPatrimonios();
+  }
+
+  Future<void> buscarPatrimonios() async{
+    carregando.value = true;
+
+    final response = await service.todos_patrimonios();
+
+    if(response.isOk) {
+      patrimonios.value = response.body ?? [];
+    }
+
+    carregando.value = false;
+  }
+}
