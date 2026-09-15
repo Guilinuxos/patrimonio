26d4341f74ca1db9d42d61e0b70bbc3449bf81d8
Guilherme Barros <guilhermecassimiro425@gmail.com>
2026-09-15 15:13:59 -0300

    continuacao da controler

--- lib/controller/patrimonio_controler.dart
+++ lib/controller/patrimonio_controler.dart
@@ -4,27 +4,68 @@ import '../service/patrimonios_service.dart';
 
 class PatrimonioControler extends GetxController {
 
-  final PatrimoniosService service = PatrimoniosService();
-
-  var patrimonios = <Patrimonios>[].obs;
-  var carregando = false.obs;
+  final service = PatrimoniosService();
+  var patrimonios = <Patrimonios>[].obs; //Lista de patrimonios
+  var isLoading = true.obs;
 
   @override
-  void onInit(){
+  void onInit() {
     super.onInit();
+    todos_patrimonios();
+    }
 
-    buscarPatrimonios();
-  }
+  Future<void> todos_patrimonios() async{ 
+    
 
-  Future<void> buscarPatrimonios() async{
-    carregando.value = true;
+
+    try {
+
+      isLoading.value = true;
+      final response = await service.todos_patrimonios();
+
+      if(response.isOk) {
+        response.body;
+      }
+      
+    } catch {
+      Get.snackbar("erro", "erro ao carregar os dados");
+    }
+    finally {
+      isLoading.value = false;
+    }
 
     final response = await service.todos_patrimonios();
 
-    if(response.isOk) {
-      patrimonios.value = response.body ?? [];
+    if(!response.isOk) {
+
     }
 
-    carregando.value = false;
+    patrimonios.value = response.body ?? [];
+
+    isLoading.value = false;
   }
+
+  Future<void> mostrar_patrimonio(String id) async{
+    isLoading.value = true;
+
+    final response = await service.mostrar_patrimonio(id);
+    var patrimonioSelecionado = Rxn<Patrimonios>();
+
+    isLoading.value = false;
+  }
+
+  Future<void> postar(Patrimonios patrimonio
+  ) async{
+    await service.postar(patrimonio);
+  }
+
+  Future<void> editar(Patrimonios patrimonio, String id
+  ) async{
+    await service.editar(patrimonio, id);
+  }
+
+  Future<void> deletar(String id) async{
+    await service.deletar(id);
+  }
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,24 @@
 import 'package:flutter/material.dart';
+import "package:get/get.dart";
+import 'package:patrimonio/controller/patrimonio_controler.dart';
 
 void main() {
 
+}
+
+
+class Teste extends StatelessWidget {
+  const new({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final controller = Get.put(PatrimonioControler());
+
+
+
+    return const Scaffold(
+      
+    );
+  }
 }
